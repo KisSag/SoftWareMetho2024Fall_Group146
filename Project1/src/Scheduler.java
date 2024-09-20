bc2a3d6b5b1139eaf091d19f8b36ffddd2d719aa
KIssag <kissagume@gmail.com>
2024-09-19 21:31:16 -0400

    Finished

--- Project1/src/Scheduler.java
+++ Project1/src/Scheduler.java
@@ -13,6 +13,7 @@ public class Scheduler {
     public static final int INDEX_PROVIDER = 6;
     public static final int INDEX_NEWTIMESLOT = 6;
     public static final int VALID_STRINGLENGTH_S_COMMAND = 7;
+    public static final int VALID_STRINGLENGTH_C_COMMAND = 6;
 
     public static final int VALIDDAY_TODAY = 0;
     public static final int VALIDDAY_BEFORETODAY = -1;
@@ -22,6 +23,8 @@ public class Scheduler {
     public static final int DATEINDEX_MONTH = 0;
     public static final int DATEINDEX_DAY = 1;
     public static final int DATEINDEX_YEAR = 2;
+
+    public static final int CALENDAR_OFFSET = 1;
 
 
     public static boolean ProgrammeRunner = true;
@@ -62,23 +65,19 @@ public class Scheduler {
                 }
                 break;
             case "C"://Cancel a appointment
-                if(cancelAppointment(CommandList)){
-                    System.out.println("Cancel Successful");
-                }else{
-                    System.out.println("Cancel Failed");
-                }
+                cancelAppointment(CommandList);
                 break;
             case "R":
                 rescheduleAppointment(CommandList);
                 break;
             case "PA":
-                outputSortedResult(AppointmentList.printByAppointment());
+                outputSortedResult(AppointmentList.printByAppointment(), "** Appointments ordered by date/time/provider **");
                 break;
             case "PP":
-                outputSortedResult(AppointmentList.printByPatient());
+                outputSortedResult(AppointmentList.printByPatient(), "** Appointments ordered by patient/date/time **");
                 break;
             case "PL":
-                outputSortedResult(AppointmentList.printByLocation());
+                outputSortedResult(AppointmentList.printByLocation(), "** Appointments ordered by county/date/time **");
                 break;
             case "PS":
                 calculateBill();
@@ -93,7 +92,7 @@ public class Scheduler {
 
         if(checkAppointmentValid(appointment)){
             AppointmentList.add(appointment);
-            System.out.println("Add Appointment Successful");
+            System.out.println(appointment.toString() + " booked");
             return true;
         }
 
@@ -119,24 +118,25 @@ public class Scheduler {
     private boolean checkAppointmentValid_DatePart(Appointment appointment){
         //if Appointment day is is correct format
         if(!appointment.getDate().isValid()){
-            System.out.println("Date not avaliable: Invalid Date Input");
+            System.out.println("Appointment date: " + appointment.getDate().toString() + " is not a valid calendar date.");
             return false;
         }
         //if Appointment day is before today
         if(checkDateValid(appointment.getDate()) <= 0){
-            System.out.println("Date not avaliable: Date can not before today or today");
+            System.out.println("Appointment date: " + appointment.getDate().toString() + " is today or a date before today.");
             return false;
         }
         //if Appointment day is out of six month
         if(checkDateValid(appointment.getDate()) == 2){
-            System.out.println("Date not avaliable: Date should within 6 month");
+            System.out.println("Appointment date: " + appointment.getDate().toString() + " is not within six months.");
             return false;
         }
         //check if Appointment day is weekend
         Calendar calndr = Calendar.getInstance();
         //SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
         try{
-            calndr = new GregorianCalendar(appointment.getDate().getYear(), appointment.getDate().getMonth(), appointment.getDate().getDay());
+            calndr = new GregorianCalendar(appointment.getDate().getYear(), appointment.getDate().getMonth() - CALENDAR_OFFSET, appointment.getDate().getDay());
+            //System.out.println(calndr.getTime());
             //calndr.setTime(sdf.parse(appointment.getDate().toString()));
         }catch(Exception e){
             System.out.println("Date can not be convert to Calendar object");
@@ -144,7 +144,7 @@ public class Scheduler {
         }
         int day = calndr.get(Calendar.DAY_OF_WEEK);
         if(day == Calendar.SATURDAY || day == Calendar.SUNDAY){
-            System.out.println("Date not avaliable: Date is Weekend");
+            System.out.println("Appointment date: " + appointment.getDate().toString() + " is Saturday or Sunday.");
             return false;
         }
         return true;
@@ -189,8 +189,24 @@ public class Scheduler {
 
         
     }
+    
     boolean cancelAppointment(String[] commandArray){
+        if(commandArray.length != VALID_STRINGLENGTH_S_COMMAND && commandArray.length != VALID_STRINGLENGTH_C_COMMAND){
+            System.out.println("Invalid Command!");
+            return false;
+        }
         Appointment TargetAppointment = null;
+
+        if(generateDate_FromString(commandArray[INDEX_APPONTMENTDATE].split("/")[DATEINDEX_MONTH], commandArray[INDEX_APPONTMENTDATE].split("/")[DATEINDEX_DAY], commandArray[INDEX_APPONTMENTDATE].split("/")[DATEINDEX_YEAR]) == null){
+            System.out.println("Appointment date: " + commandArray[INDEX_APPONTMENTDATE] + " is not a valid date.");
+            return false;
+        }else if(generateTimeSlot_FromString(commandArray[INDEX_TIMESLOT]) == null){
+            System.out.println(commandArray[INDEX_TIMESLOT] + " is not a valid time slot.");
+            return false;
+        }else if(generateProfile_FromString(commandArray[INDEX_FIRSTNAME], commandArray[INDEX_LASTNAME], commandArray[INDEX_BIRTHDAY]) == null){
+            return false;
+        }
+
         try{
             TargetAppointment = AppointmentList.getAppointment_byCondition(generateDate_FromString(commandArray[INDEX_APPONTMENTDATE].split("/")[DATEINDEX_MONTH], commandArray[INDEX_APPONTMENTDATE].split("/")[DATEINDEX_DAY], commandArray[INDEX_APPONTMENTDATE].split("/")[DATEINDEX_YEAR]),generateTimeSlot_FromString(commandArray[INDEX_TIMESLOT]), generateProfile_FromString(commandArray[INDEX_FIRSTNAME], commandArray[INDEX_LASTNAME], commandArray[INDEX_BIRTHDAY]));
         }catch(Exception e){
@@ -198,15 +214,18 @@ public class Scheduler {
         }
         
         if(TargetAppointment == null){
+            System.out.println(commandArray[INDEX_APPONTMENTDATE] + " " + generateTimeSlot_FromString(commandArray[INDEX_TIMESLOT]).getString_TimeSlot() + " " + commandArray[INDEX_FIRSTNAME] + " " + commandArray[INDEX_LASTNAME] + " " + commandArray[INDEX_BIRTHDAY] + " does not exist");
             return false;
         }else{
             AppointmentList.remove(TargetAppointment);
+            System.out.println(commandArray[INDEX_APPONTMENTDATE] + " " + generateTimeSlot_FromString(commandArray[INDEX_TIMESLOT]).getString_TimeSlot() + " " + commandArray[INDEX_FIRSTNAME] + " " + commandArray[INDEX_LASTNAME] + " " + commandArray[INDEX_BIRTHDAY] + " has been canceled");
             return true;
         }
     }
     Appointment generateAppointment(String[] commandArray){
 
         if(commandArray.length != VALID_STRINGLENGTH_S_COMMAND){
+            System.out.println("Invalid Command!");
             return null;
         }
 
@@ -217,6 +236,11 @@ public class Scheduler {
 
         try{
             date = generateDate_FromString(commandArray[INDEX_APPONTMENTDATE].split("/")[DATEINDEX_MONTH], commandArray[INDEX_APPONTMENTDATE].split("/")[DATEINDEX_DAY], commandArray[INDEX_APPONTMENTDATE].split("/")[DATEINDEX_YEAR]);
+        }catch(Exception e){
+            System.out.println("Appointment date: " + commandArray[INDEX_APPONTMENTDATE] + " is not a valid date.");
+            return null;
+        }
+        try{
             slot = generateTimeSlot_FromString(commandArray[INDEX_TIMESLOT]);
             patient = generateProfile_FromString(commandArray[INDEX_FIRSTNAME], commandArray[INDEX_LASTNAME], commandArray[INDEX_BIRTHDAY]);
             provider = generateProvider_FromString(commandArray[INDEX_PROVIDER]);
@@ -225,6 +249,10 @@ public class Scheduler {
         }
         
         //Invalid Generate
+        if(date == null){
+            System.out.println("Appointment date: " + commandArray[INDEX_APPONTMENTDATE] + " is not a valid date.");
+        }
+
         if(date == null || slot == null || patient == null || provider == null){
             return null;
         }
@@ -247,7 +275,7 @@ public class Scheduler {
                 continue;
             }
             
-            System.out.println("(" +Integer.toString(i) +  ") " + PatientList[i].toString());
+            System.out.println("(" +Integer.toString(i + 1) +  ") " + PatientList[i].toString());
             
         }
         System.out.println("** End of List **");
@@ -255,7 +283,6 @@ public class Scheduler {
         AppointmentList.cleanList();
 
     }
-    
     
     
     private Date generateDate_FromString(String Month, String Day, String Year){
@@ -268,13 +295,24 @@ public class Scheduler {
         return date;
     }
     private Profile generateProfile_FromString(String fn, String ln, String Date_String){
-        Date date = generateDate_FromString(Date_String.split("/")[DATEINDEX_MONTH], Date_String.split("/")[DATEINDEX_DAY], Date_String.split("/")[DATEINDEX_YEAR]);
-        if(date == null || checkDateValid(date) > 0 || !date.isValid()){
-            System.out.println("Invalid Patient Birthday");
+
+        Date date = null;
+        try{
+            date = generateDate_FromString(Date_String.split("/")[DATEINDEX_MONTH], Date_String.split("/")[DATEINDEX_DAY], Date_String.split("/")[DATEINDEX_YEAR]);
+        }catch(Exception e){
+        }
+
+        if(date == null || !date.isValid()){
+            System.out.println("Patien dob: " + Date_String + " is not a valid date.");
+            return null;
+        }
+        if(checkDateValid(date) > 0){
+            System.out.println("Patien dob: " + Date_String + " is today or a date after today.");
             return null;
         }
 
         Profile profile;
+
         try{
             profile = new Profile(fn, ln, date);
         }catch(Exception e){
@@ -285,8 +323,16 @@ public class Scheduler {
     }
     private Timeslot generateTimeSlot_FromString(String Slot){
 
-        int ENUMIndex = Integer.parseInt(Slot);
+        int ENUMIndex;
+        try{
+            ENUMIndex = Integer.parseInt(Slot);
+        }catch(Exception e){
+            System.out.println(Slot + " is not a valid time slot.");
+            return null;
+        }
+
         if(ENUMIndex < 1 || ENUMIndex > 6){
+            System.out.println(Slot + " is not a valid time slot.");
             return null;
         }
 
@@ -302,6 +348,7 @@ public class Scheduler {
                 return provider_Iterator;
             }
         }
+        System.out.println(provider_String + "- provider doesn't exist");
         return null;
     }
 
@@ -330,7 +377,7 @@ public class Scheduler {
         int INDEX_OFFSET = 1;
 
         Calendar calndr = Calendar.getInstance();
-        calndr.setLenient(false);
+        //calndr.setLenient(false);
         calndr.add(Calendar.MONTH, MonthOffset + INDEX_OFFSET);
         calndr.add(Calendar.DATE, DayOffset);
         calndr.add(Calendar.YEAR, YearOffset);
@@ -340,16 +387,24 @@ public class Scheduler {
         return new Date(calndr.get(Calendar.MONTH), calndr.get(Calendar.DAY_OF_MONTH), calndr.get(Calendar.YEAR));
     }
 
-    void outputSortedResult(Appointment[] AppontArr_Clean){
+    
+    
+    void outputSortedResult(Appointment[] AppontArr_Clean, String beginningIntro){
+        
         if(AppontArr_Clean == null || AppontArr_Clean.length == 0){
             System.out.println("Schedule Canlendat is empty");
             return;
         }
+
+        System.out.println(beginningIntro);
+
         for(int i = 0; i < AppontArr_Clean.length; i += 1){
             if(AppontArr_Clean[i] == null){
                 return;
             }
             System.out.println(AppontArr_Clean[i].toString() + "");
         }
+
+        System.out.println("**end of list**");
     }
 }
