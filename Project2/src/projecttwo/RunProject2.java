67f88b459512ce8befc7a9bc4b90bc8da20997fc
J-JHsu <jh555bra@gmail.com>
2024-10-16 21:35:28 -0400

    Update RunProject2.java

--- Project2/src/projecttwo/RunProject2.java
+++ Project2/src/projecttwo/RunProject2.java
@@ -1,8 +1,12 @@
 package projecttwo;
 
 /**
+ * The RunProject2 class starts the ClinicManager, which manages the clinic's scheduling system.
+ * This class contains the main method to run the application.
+ * 
  * {@code @author:} Tianxiang Huang
  */
+
 public class RunProject2 {
     public static void main(String [] args) {
         new ClinicManager().run();
